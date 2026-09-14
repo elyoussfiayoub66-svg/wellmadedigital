c9f2fec92d8e052d6bdaf8f8c59f9afc7ceb4e4f
AYOUB <elyoussfiayoub66@gmail.com>
2026-09-14 17:51:52 +0100

    Remove FeaturesShowcase from clinique landing page

--- app/clinique/page.jsx
+++ app/clinique/page.jsx
@@ -5,7 +5,6 @@ import { useState, useEffect } from 'react';
 import { ArrowRight, CheckCircle2, Shield, Zap } from 'lucide-react';
 import { submitCliniqueBooking } from '@/app/actions/clinique';
 import * as meta from '@/lib/tracking/meta';
-import FeaturesShowcase from '@/components/home/FeaturesShowcase';
 import FAQ from '@/components/home/FAQ';
 
 import { useRouter } from 'next/navigation';
@@ -221,7 +220,6 @@ export default function CliniqueLP() {
       </section>
 
       {/* 5. SOLUTIONS */}
-      <FeaturesShowcase />
       <FAQ />
 
 
